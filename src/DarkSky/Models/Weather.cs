0743877e310e90ef27fd26929e897c9c7894f6d5
Anne Belka and Ryan Peterson <anne+ryan@>
2017-02-27 11:51:59 -0800

    get basic reponse from darksy api

---
+++ src/DarkSky/Models/Weather.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DarkSky.Models
+{
+    public class Weather
+    {
+
+        public string Summary { get; set; }
+        public string Temperature { get; set; }
+
+        public Weather()
+        {
+
+        }
+
+        public void GetWeather()
+        {
+            var client = new RestClient("https://api.darksky.net/");
+            var request = new RestRequest("forecast/90da12a86306b1ec09bd65356b7e0707/37.8267,-122.4233", Method.GET);
+            var response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var daily = JsonConvert.DeserializeObject<Weather>(jsonResponse["daily"].ToString());
+            
+            Console.WriteLine("Summary: {0}", daily.Summary);
+            //Console.WriteLine(": {0}", message.From);
+            //Console.WriteLine("Body: {0}", message.Body);
+            //Console.WriteLine("Status: {0}", message.Status);
+            
+            Console.ReadLine();
+
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+
+    }
+}
---
+++ src/DarkSky/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace DarkSky
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath);
+            Configuration = builder.Build();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
